6b38437e0ef27a8e4e12b71540709b723ea8d5b3
Huey <huey.he@dji.com>
2017-05-10 11:31:11 +0800

    docs: 更新作者信息

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 CAFUE Inc. All rights reserved.
+ * Copyright 2017 hueyhe. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
